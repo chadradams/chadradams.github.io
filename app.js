87eacce62b21e39c68b7e3fa044a0fbb4a7baffb
Chad Adams <chadada@microsoft.com>
2023-02-19 04:41:30 +0000

    js update

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.remove('hidden');
+      entry.target.classList.add('show');
     } else {
         entry.target.classList.add('hidden');
+        entry.target.classList.remove('show');
     }
   });
 });
